09fb62159ce2d98238d5a88bd8ce115c8fe96978
samisci <47791400+samisci@users.noreply.github.com>
2019-07-24 23:28:16 +0300

    Add files via upload

---
+++ ExceptionMakers.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class ExceptionMakers
+    {
+        public Exception HackerUDivideByZero = new Exception("div by zero");
+        public Exception HackerUFormatException = new Exception("HackerUFormat");
+        public Exception HackerUIndexOutOfRangeException = new Exception("HackerUIndexOutOfRangeException");
+        
+        public int GetNumber()
+
+        {
+            try
+            {
+                return int.Parse(Console.ReadLine());
+            }
+            catch (FormatException ex)
+            {
+                throw HackerUFormatException;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int[] GetNumbers()
+        {
+            int[] numbers = new int[4];
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    numbers[i] = int.Parse(Console.ReadLine());
+                }
+
+                return numbers;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw HackerUIndexOutOfRangeException;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void DivideByZero()
+        {
+            try
+            {
+                int x = 0;
+                x = 10 / x;
+            }
+            catch (DivideByZeroException ex)
+            {
+                
+                throw HackerUDivideByZero;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a number between 1 - 3: ");
+                int n = int.Parse(Console.ReadLine());
+
+                ExceptionMakers em = new ExceptionMakers();
+
+                switch (n)
+                {
+                    case 1: em.GetNumber(); break;
+                    case 2: em.GetNumbers(); break;
+                    case 3: em.DivideByZero(); break;
+                }
+            }
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine("You must enter a valid number");
+            //}
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    Console.WriteLine("You have passed the length of the array");
+            //}
+            //catch (DivideByZeroException) 
+            //{
+            //    Console.WriteLine("You have divided a number by zero");
+            //}
+            catch (Exception ex)
+            {
+                Console.WriteLine("We've got a generic exception. The exception message is " + ex.Message);
+                
+            }
+        }
+    }
+}
